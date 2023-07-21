656bef37c7e3f240d343b5bf2ec0f796a3027f96
programmer584 <96943997+programmer584@users.noreply.github.com>
2023-07-21 17:33:30 +0200

    New website

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-$(".nav_toggle").on("click", function () {
-    if ($(".nav_item").hasClass("active")) {
-    $(".nav_item").removeClass("active");
-    } else {
-    $(".nav_item").addClass("active");
-    }
-});
-});
